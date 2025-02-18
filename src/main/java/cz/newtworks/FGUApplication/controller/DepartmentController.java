c0e7de88c2765312958b55d649c87a86d382650b
Newt <tfisera1997@gmail.com>
2025-02-18 16:31:43 +0100

    Rozprazované oddělení (chyba při zakládání oddělení)

--- src/main/java/cz/newtworks/FGUApplication/controller/DepartmentController.java
+++ src/main/java/cz/newtworks/FGUApplication/controller/DepartmentController.java
@@ -1,9 +1,22 @@
 package cz.newtworks.FGUApplication.controller;
 
+import cz.newtworks.FGUApplication.dto.DepartmentDTO;
+import cz.newtworks.FGUApplication.service.DepartmentService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/department")
 public class DepartmentController {
+
+    @Autowired
+    private DepartmentService departmentService;
+
+    @PostMapping("/create")
+    public DepartmentDTO addDepartment(@RequestBody DepartmentDTO departmentDTO){
+       return departmentService.addDepartment(departmentDTO);
+    }
 }
--- src/main/java/cz/newtworks/FGUApplication/service/DepartmentServiceImpl.java
+++ src/main/java/cz/newtworks/FGUApplication/service/DepartmentServiceImpl.java
@@ -1,7 +1,39 @@
 package cz.newtworks.FGUApplication.service;
 
+import cz.newtworks.FGUApplication.dto.DepartmentDTO;
+import cz.newtworks.FGUApplication.dto.mapper.DepartmentMapper;
+import cz.newtworks.FGUApplication.entity.DepartmentEntity;
+import cz.newtworks.FGUApplication.entity.repository.DepartmentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class DepartmentServiceImpl implements DepartmentService{
+
+    @Autowired
+    private DepartmentMapper departmentMapper;
+
+    @Autowired
+    private DepartmentRepository departmentRepository;
+
+    @Override
+    public DepartmentDTO addDepartment(DepartmentDTO departmentDTO) {
+        DepartmentEntity newDepartment = departmentMapper.toEntity(departmentDTO);
+
+        departmentRepository.save(newDepartment);
+
+        return departmentDTO;
+    }
+
+    @Override
+    public List<DepartmentDTO> getAllDepartments() {
+        return departmentRepository.findAll()
+                .stream()
+                .map(i -> departmentMapper.toDTO(i))
+                .collect(Collectors.toList());
+    }
+
 }
