1f88c2a354e6ec75cf76abc64ea72a5fa61b7b45
Newt <tfisera1997@gmail.com>
2025-03-04 17:19:33 +0100

    Automatické vytvoření vazby manyToMany + nefunkční vazební tabulka

--- src/main/java/cz/newtworks/FGUApplication/entity/PersonEntity.java
+++ src/main/java/cz/newtworks/FGUApplication/entity/PersonEntity.java
@@ -5,6 +5,8 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
+
 
 @Entity
 @Table(name = "people")
@@ -31,4 +33,11 @@ public class PersonEntity {
     private Role role;
 
     //vazba mezi tabulkami - department
+    @ManyToMany
+    @JoinTable(
+            name = "person_department",
+            joinColumns = @JoinColumn(name = "person_id"),
+            inverseJoinColumns = @JoinColumn(name = "department_id")
+    )
+    List<DepartmentEntity> departments;
 }
--- target/generated-sources/annotations/cz/newtworks/FGUApplication/dto/mapper/CouncilMapperImpl.java
+++ target/generated-sources/annotations/cz/newtworks/FGUApplication/dto/mapper/CouncilMapperImpl.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-03-03T15:27:06+0100",
+    date = "2025-03-04T17:07:07+0100",
     comments = "version: 1.6.3, compiler: javac, environment: Java 23.0.2 (Oracle Corporation)"
 )
 @Component
