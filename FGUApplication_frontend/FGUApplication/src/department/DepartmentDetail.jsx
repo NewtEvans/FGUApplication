a8cf742844c0010ab3239bb6b99124fbbcee3608
Newt <tfisera1997@gmail.com>
2025-04-14 12:17:13 +0200

    Upraven detail oddělení - výpis všech lidí na odd

--- FGUApplication_frontend/FGUApplication/src/department/DepartmentDetail.jsx
+++ FGUApplication_frontend/FGUApplication/src/department/DepartmentDetail.jsx
@@ -2,10 +2,12 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router";
 import { apiGet } from "../utils/api";
 import { Link } from "react-router";
+import PersonTable from "../person/PersonTable";
 
 const DepartmentDetail = () => {
   const { id } = useParams();
   const [department, setDepartment] = useState({});
+  const [people, setPeople] = useState([]);
 
   useEffect(() => {
     apiGet("/department/detail/" + id)
@@ -15,13 +17,20 @@ const DepartmentDetail = () => {
       .catch((error) => {
         console.log(error);
       });
+    apiGet("/person/info/" + id).then((data) =>
+      setPeople(data).catch((error) => console.log(error))
+    );
   }, []);
 
   return (
     <div>
       <h1>Detail oddělení</h1>
       <hr />
-      <h2>{department.departmentName}</h2>
+      <h2>
+        {department.departmentName} (odd. {department.departmentNumber})
+      </h2>
+
+      <PersonTable people={people} />
 
       <Link to={`/departments/edit/${id}`} className="btn btn-md btn-warning">
         Upravit oddělení
