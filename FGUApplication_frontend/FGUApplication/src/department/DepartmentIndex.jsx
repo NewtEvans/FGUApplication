55dd9c926b4c510ae16c723691cc525e32b97130
Newt <tfisera1997@gmail.com>
2025-05-19 11:17:48 +0200

    Uprava departmentIndex a pridani paginace

--- FGUApplication_frontend/FGUApplication/src/department/DepartmentIndex.jsx
+++ FGUApplication_frontend/FGUApplication/src/department/DepartmentIndex.jsx
@@ -8,9 +8,24 @@ const DepartmentIndex = () => {
   const [url, setUrl] = useState("/department/all");
   const [departments, setDepartments] = useState([]);
 
+  const [page, setPage] = useState(0);
+  const [totalPages, setTotalPages] = useState();
+  const [size, setSize] = useState(10);
+
   useEffect(() => {
-    apiGet(url).then((data) => setDepartments(data));
-  }, []);
+    apiGet(url, {
+      page: page,
+      size: size,
+      totalPages: totalPages,
+    })
+      .then((data) => {
+        setDepartments(data.content);
+        setTotalPages(data.totalPages);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, [url, page, size, totalPages]);
 
   return (
     <div>
@@ -23,6 +38,35 @@ const DepartmentIndex = () => {
       </div>
       <br />
       <DepartmentTable departments={departments} />
+      <div className="d-flex justify-content-between">
+        <button
+          className="btn btn-primary btn-sm"
+          disabled={page === 0}
+          onClick={() => setPage(page - 1)}
+        >
+          Předchozí
+        </button>
+        <select
+          id="pageSize"
+          className="form-select w-auto d-inline-block"
+          onChange={(e) => {
+            setPage(0);
+            setSize(parseInt(e.target.value));
+          }}
+        >
+          <option value="10">10</option>
+          <option value="25">25</option>
+          <option value="50">50</option>
+          <option value="100">100</option>
+        </select>
+        <button
+          className="btn btn-primary btn-sm"
+          disabled={page + 1 >= totalPages}
+          onClick={() => setPage(page + 1)}
+        >
+          Další
+        </button>
+      </div>
     </div>
   );
 };
